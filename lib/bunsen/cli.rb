5ac99d4200fb2aa945a25079fb74dc8a4b444c5a
Richard Henning <rhenning@neat.com>
2014-06-26 13:43:23 -0400

    Add options to skip reading indexes or data

--- lib/bunsen/cli.rb
+++ lib/bunsen/cli.rb
@@ -31,8 +31,24 @@ module Bunsen
       desc: "List of indexes to read [all]",
       type: :array,
       default: []
+    option :skip_collections,
+      desc: "Skip reading collection data",
+      type: :boolean
+    option :skip_indexes,
+      desc: "Skip reading indexes",
+      type: :boolean
     
     def warm
+      if options[:skip_collections] && options[:collections].any?
+        say "Choose one: --skip-collections or --collections"
+        exit 1
+      end
+
+      if options[:skip_indexes] && options[:indexes].any?
+        say "Choose one: --skip-indexes or --indexes"
+        exit 1
+      end
+
       warmer = Bunsen::Warmer.new(
         options[:database],
         options[:collections],
@@ -43,22 +59,26 @@ module Bunsen
 
       Formatador.display_line "[cyan]#{Bunsen::version_string}[/] warming mongodb://#{options[:host]}:#{options[:port]}/#{options[:database]}"
       
-      Formatador.display_line "[yellow]warming data:[/] #{options[:collections].join(' ')}"
-      warmer.touch_each_collection do |_, completed_count, remaining_count|
-        Formatador.redisplay_progressbar(
-          completed_count,
-          remaining_count,
-          color: "red"
-        )
+      unless options[:skip_collections]
+        Formatador.display_line "[yellow]warming data:[/] #{options[:collections].join(' ')}"
+        warmer.touch_each_collection do |_, completed_count, remaining_count|
+          Formatador.redisplay_progressbar(
+            completed_count,
+            remaining_count,
+            color: "red"
+          )
+        end
       end
 
-      Formatador.display_line "[yellow]warming indexes:[/] #{options[:indexes].join(' ')}"
-      warmer.touch_each_index do |_, completed_count, remaining_count|
-        Formatador.redisplay_progressbar(
-          completed_count,
-          remaining_count,
-          color: "red"
-        )
+      unless options[:skip_indexes]
+        Formatador.display_line "[yellow]warming indexes:[/] #{options[:indexes].join(' ')}"
+        warmer.touch_each_index do |_, completed_count, remaining_count|
+          Formatador.redisplay_progressbar(
+            completed_count,
+            remaining_count,
+            color: "red"
+          )
+        end
       end
 
       Formatador.display_line "[green]all done![/]"      
